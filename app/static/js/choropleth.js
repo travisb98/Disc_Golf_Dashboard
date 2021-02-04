89b86d4c75c1843e4290d8f0112c317d6eaee106
unknown <travisbostick@gmail.com>
2021-02-03 20:25:35 -0600

    made choropleth work with API

--- app/static/js/choropleth.js
+++ app/static/js/choropleth.js
@@ -6,37 +6,42 @@
 // })
 
 
-///// variable unpacking function used with actual api this version works with the actual api
+///// function for returning all the variables used in multiple other functions
 function unpacker(data){
+
+
+    /////// i need to create 2 input variables. One for the feature and one for the aggregate function(mean, etc)
+
     /// makes a list of the state abbreviations that will be used on the pop ups
-    var locations = data.map(item => item.state_abbr);
+    var locations = data.data.map(item => item.state_abbr);
     // console.log(locations);
 
-    // ///// text for the pop-up when hovering over a state
-    var text = data.map(item => item.state_name);
+    ///// text for the pop-up when hovering over a state
+    var text = data.data.map(item => item.state_name);
     // console.log(text);
 
-    // // gets the label for the data points called from the api(ie holes)
-    var primary_label=Object.keys(data[0])[2];
-    // console.log(primary_label);
-   
+    // gets the label for the first data point called from the api
+    var primary_label=data.primary_label;
+
+
     /// gets a list of the main feature well be graphing
-    var primary_feature_list = data.map(item => item[primary_label]);
+    ///// remove parsing once api changes
+    var primary_feature_list = data.data.map(item => parseFloat(item.primary_feature));
     // console.log(primary_feature_list);
 
     //// finding the max number in the data set
     var max = Math.max.apply(Math,primary_feature_list);
-    // console.log(max);
+
     //// finding the min number in the data set
     var min = Math.min.apply(Math,primary_feature_list);
-    // console.log(min);
 
-    // //// finding the state name with the max number in the data set
-    var topStateName = data.find(item => item[primary_label] == max).state_name;
+    //// finding the state name with the max number in the data set
+    var topStateName = data.data.find(item => item.primary_feature === max).state_name;
+
     // console.log(topStateName);
 
     ///// finding the state with the min number in the data set
-    var bottomStateName = data.find(item => item[primary_label] == min).state_name;
+    var bottomStateName = data.data.find(item => item.primary_feature == min).state_name;
     // console.log(bottomStateName);
 
     return [locations, text, primary_label, primary_feature_list, max,min, topStateName, bottomStateName];
@@ -51,6 +56,9 @@ function mapDataLayout(data){
 
     /// simplified green yellow red color scale
     var colorScale = [[0, 'green'], [0.5, 'yellow'],[1, 'red']];
+
+    /// simplified green yellow red color scale
+    var colorScale = [[0, 'white'],[1, 'FF8000']];
 
     ///// data for the choropleth map
     var mapData = [{
@@ -128,13 +136,17 @@ function updateChoro(data){
 
 
 // ///// api that runs on start, creating the choropleth and the info card
-d3.json("/api/v1/FeatureAggregate?feature=rating&aggregate=mean").get(function(error, data) {
+d3.json("/api/v1/FeatureAggregate?feat1=holes&feat2=rating").get(function(error, data) {
     console.log("Start up api call")
+
+    // console.log(data);
     // this function will make the choropleth map
     makeTheChoro(data);
 
     //// this makes the info pan with the highest and lowest stat
     choroPane(data);
+    //// here for testing
+    // unpacker(data);
 
 });//// I should add error handling here
 
@@ -151,7 +163,8 @@ filter_button.on('click',function(){
     ////// also will probably need variables for filtering
 
 
-    d3.json("/api/v1/FeatureAggregate?feature=holes&aggregate=mean").get(function(error, data){
+
+    d3.json("/api/v1/FeatureAggregate?feat1=rating&feat2=holes").get(function(error, data){
 
         console.log("Second API call triggered button")
         // console.log(data);
@@ -162,7 +175,7 @@ filter_button.on('click',function(){
 
         ///// fucntion that updates the choropleth map
         updateChoro(data);
-
+        
     });
 
 });
