034e79eb5226e6727929536aa2d34169d49c177c
ConnorHz <hertzconnor@gmail.com>
2021-02-03 20:33:11 -0600

    Adding form updates

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,11 +1,16 @@
 
+function filterData() {
+    d3.event.preventDefault();
+
+    d3.select("#startDate").property("value")
+
+    d3.json("/api/v1/FeatureAggregate?feature=holes&aggregate=mean")
+    .header("holes", 19)
+    .get(function(error, data) {
+        console.log(data);
+    })
+
+}
 
 
-d3.json("/api/v1/FeatureAggregate?feature=holes&aggregate=mean")
-.header("holes", 19)
-.get(function(error, data) {
-    console.log(data);
-})
-
-
-
+d3.select("#filter-btn").on("click", filterData);
