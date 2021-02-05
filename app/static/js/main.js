dc4521f1fff61b88478a580c89e9c348b3ac5d6a
unknown <travisbostick@gmail.com>
2021-02-05 12:32:31 -0600

    dynamic feature removal is working

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -4,7 +4,7 @@ function getFilters() {
 
     var sidebar = d3.select(".sidebar");
 
-    // console.log(sidebar);
+    console.log(sidebar);
 
     return JSON.stringify(filters);
 }
@@ -15,17 +15,14 @@ d3.json("/api/v1/FeatureAggregate?feat1=holes&feat2=rating")
 .get(function(error, data) {
     // error handling 
     if (error) return console.warn(error);
-    // console.log("Start up api call");
-    // console.log(data);
-    //// makes choropleth map
+
+    //// create choropleth
     mapDataLayout(data);
 
     //// makes bar graph
     updateBar(data);
 
 });
-
-
 
 ///// defining the filter button. 
 var filter_button = d3.select("#filter-btn");
@@ -37,18 +34,14 @@ var primaryUserSelection= d3.select("#primarySelection");
 var secondaryUserSelection = d3.select('#secondarySelection');
 
 
-///// for some reason this works for every single option except for "Average Number of Holes with Water" and i'm not really sure why. super weird
-///// this event listener is only on the primary feature dropdown, we'll need to make a similar one on the secondary feature dropdown
-///// instead of repeating the code below for the primaryUserSelection, maybe i can make the secondaryUserSelection inactive until the primaryUserSelection changes
+/// used for avoiding duplicate features
 ///// event listener used when primary feature changes:
 primaryUserSelection.on('change',function(){ 
-
 
     //// removes all options from the secondary dropdown
     secondaryUserSelection.selectAll("*").remove();
 
     //// looping through the options in the primary feature menu
-    //// change this to length
     for (var j = 0; j < this.length; j++) {
 
         ///// grabs the current option tag from the primary user selection
@@ -59,22 +52,10 @@ primaryUserSelection.on('change',function(){
         .attr('value',current_option.value)
         .attr('id',current_option.id)
         .html(current_option.text);
-
     };
-
-
-
-    /////// gets the value of the option that was picked in the primary dropdown
-    var prim_val = this.value;
-
     ///// removes the option from secondary list that matches the option selected in the primary dropdown
-    //// this works for every option except "Average Number of Holes with Water" and I have no idea why and it's very weird
-    secondaryUserSelection.select(`#${prim_val}`).remove();
-
-
+    secondaryUserSelection.select(`#${this.value}`).remove();
 });
-
-
 
 
 ///// when the filter button gets clicked....
@@ -86,24 +67,13 @@ filter_button.on('click',function(){
     /////// grabs the secondary value from the selection
     var secondaryUserValue = secondaryUserSelection.node().value;
 
-
-    ////// temporary error handling to deal with "average number of holes error"
-    if (primaryUserValue == secondaryUserValue){
-
-        var t = "features can't be the same, working on making this impossible. issue with Average Number of Holes with Water ";
-        console.log(t);
-        alert(t);
-        ///// exits the function, skipping api call
-        return false;
-    };
-
-
     ///.....api call based on the values the users selected
     d3.json(`/api/v1/FeatureAggregate?feat1=${primaryUserValue}&feat2=${secondaryUserValue}`)
     .get(function(error, data){
 
         ///// fucntion that updates the choropleth map
         mapDataLayout(data);
+
         
         
     });
